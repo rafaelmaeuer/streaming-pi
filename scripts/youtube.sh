82ede30f5585a9f68cf847f07700025ced6aabae
Rafael M <github@rafaelmaeuer.de>
2022-10-17 20:20:53 +0200

    fix picam service status for restart

--- scripts/youtube.sh
+++ scripts/youtube.sh
@@ -6,7 +6,7 @@ do echo "waiting for nginx"; sleep 1;
 done
 
 echo "check if picam is recording"
-if [[ $(systemctl is-active picam) == "inactive" ]]; then
+if [[ $(systemctl is-active picam) == "failed" ]]; then
     /home/pi/shutdown.sh reboot
 else
     rm -f /home/pi/Documents/count.txt
